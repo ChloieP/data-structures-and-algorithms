8547da2854eb7aacd60344b34658c8a635b112e4
Chloie Parsons <chloieparsons@gmail.com>
2019-04-25 16:40:55 -0700

    Challenge 1 PASSED

--- code-challenges/301/sort/challenges-03.test.js
+++ code-challenges/301/sort/challenges-03.test.js
@@ -6,7 +6,9 @@ Write a function named sortBackwards that takes in an array of numbers and retur
 ------------------------------------------------------------------------------------------------ */
 
 const sortBackwards = (arr) => {
-  // Solution code here...
+  arr.sort((a, b) => b - a);
+
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
