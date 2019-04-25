440e01d51be2c70c04f1781bf560fece3e0827d2
Chloie Parsons <chloieparsons@gmail.com>
2019-04-25 16:44:11 -0700

    Challenge 3 PASSED

--- code-challenges/301/sort/challenges-03.test.js
+++ code-challenges/301/sort/challenges-03.test.js
@@ -30,7 +30,9 @@ Write a function named sortByLength that takes in an array of strings and return
 ------------------------------------------------------------------------------------------------ */
 
 const sortByLength = (arr) => {
-  // Solution code here...
+  arr.sort((a, b) => a.length - b.length);
+
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
