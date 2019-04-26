18aa8c0043092dfb1e9f24f6416ecab0affd259d
Chloie Parsons <chloieparsons@gmail.com>
2019-04-26 07:45:25 -0700

    Challenge 8 PASSED

--- code-challenges/301/sort/challenges-03.test.js
+++ code-challenges/301/sort/challenges-03.test.js
@@ -115,7 +115,35 @@ If two people have the same full name, the younger one should come first. Do not
 ------------------------------------------------------------------------------------------------ */
 
 const sortPeopleBetter = (arr) => {
-  // Solution code here...
+  arr.sort((a,b) => {
+    if (a.lastName < b.lastName){
+      return -1;
+
+    } else if (a.lastName > b.lastName){
+      return 1;
+
+    } else { 
+    
+    if (a.firstName < b.firstName){
+      return -1;
+
+    } else if (a.firstName > b.firstName) {
+      return 1;
+
+    } else {
+
+    if (a.age < b.age){
+      return -1;
+
+    } else if (a.age > b.age){
+      return 1;
+
+    }
+      return 0;
+    }}
+  })
+
+    return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
