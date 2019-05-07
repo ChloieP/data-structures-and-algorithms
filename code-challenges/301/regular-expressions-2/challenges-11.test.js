edc3e66749b5713f04c25df1f287afd16fcad2b2
Chloie Parsons <chloieparsons@gmail.com>
2019-05-07 14:56:32 -0700

    Challenge 2 PASSED

--- code-challenges/301/regular-expressions-2/challenges-11.test.js
+++ code-challenges/301/regular-expressions-2/challenges-11.test.js
@@ -24,7 +24,7 @@ Note: if you ever need to validate an email using a regex in practice, the Inter
 ------------------------------------------------------------------------------------------------ */
 
 const validateEmail = (email) => {
-  return /\S+@\S+\.\S+/g.test(email); // WORKING ON THIS
+  return /^(\w+)(.\w+)?@(\w+)\.(com|net|org)\b/g.test(email);
 };
 
 /* ------------------------------------------------------------------------------------------------
