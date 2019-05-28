47d646c11cf74bce60ea090a794c4082d5eae7df
Chloie Parsons <chloieparsons@gmail.com>
2019-05-27 17:53:02 -0700

    WOOHOO Working solution, per codepen.io

--- code-challenges/401/arrayReverse/array-reverse.js
+++ code-challenges/401/arrayReverse/array-reverse.js
@@ -1,18 +1,19 @@
 // Write a function that takes an array as an argument. Without utilizing any of the methods in my language, return an array with elements in reverse order.
 
-const arr = [1, 2, 3, 4, 5, 6];
+let reverseArray = (arr) => { 
+  let initial = 0;
+  let lastElement = arr.length -1;
 
-const initial = 0;
-const lastElement = arr.length -1;
+  while (initial < lastElement){
+    let x = arr[lastElement];
+    arr[lastElement] = arr[initial];
+    arr [initial] = x;
+    initial ++;
+    lastElement --;
+  }
 
-while (inital < lastElement)
-  {var x = arr[i];
-  arr[initial] = arr[lastElement];
-  arr [lastElement] = x;
-  initial ++;
-  lastElement --;
-}
-
+  return arr;
+};
 
 
 
