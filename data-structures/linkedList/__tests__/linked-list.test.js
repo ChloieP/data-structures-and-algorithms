5212f0535d1745a4c1c154b2902e2099240171bf
Chloie Parsons <chloieparsons@gmail.com>
2019-06-03 14:53:39 -0700

    Boooyahgit add . Tests passed.

--- data-structures/linkedList/__tests__/linked-list.test.js
+++ data-structures/linkedList/__tests__/linked-list.test.js
@@ -1,28 +1,128 @@
 'use strict';
 
-const LinkedList = require('../files/linked-list.js');
+const linkedList = require('../files/linked-list.js');
 
 describe('Tests for the linked list', () => {
-  let list = null;
-  
-  beforeEach(function() {
-    list = new LinkedList();
-  });
-  
-  it('can instantiate an empty list', () => {
-    expect(list.head).toBeNull();
-  });  
-    
-  it('can properly insert into the linked list', () => {
-    list.add('bippity');
-    expect(list.head).not.toBeNull();
-    expect(list.value).not.toBeNull();
-    expect(list.head.data).toEqual('bippity');
-    expect(list.value.data).toEqual('bippity');
+  let list;
+  beforeEach(() => {
+    list = new linkedList();
   });
 
-  it('can properly points to the first node in the linked list using the head property', () => {
-    expect(list.head).toBeTruthy();
-    expect(list.head.next).not.toBeNull();
+  it('successfully instantiates an empty linked list', () => {
+    expect(list).toBeDefined();
+  });
+
+  describe('Insert', () => { 
+
+    it('should take a value and return that value', () => {
+      expect(list.insert(3)).toEqual(3);
+    });
+
+    it('can properly insert multiple nodes', () => {
+      list.insert(26);
+      list.insert(13);
+      let current = list.head;
+      expect(current.val).toEqual(13);
+      expect(current.next.val).toEqual(26);
+    });
+  });
+
+  describe('Include', () => { 
+
+    it('should take a value and return true if value is found', () => {
+      list.insert(26);
+      list.insert(13);
+      expect(list.includes(13)).toEqual(true);
+      expect(list.includes(99)).toEqual(false);
+    });
+  });
+
+  describe('Print', () => { 
+
+    it('should console log all values in linked list', () => {
+      let mockConsoleLog = jest.spyOn(console, 'log');
+
+      mockConsoleLog.mockImplementation(() => true);
+
+      list.print();
+      expect(mockConsoleLog).not.toBeCalled();
+    });
+  });
+
+  describe('Append', () => { 
+
+    it('appends node to the end of the linked list', () => {
+      list.insert(1);
+      list.insert(2);
+      list.append(3);
+
+      let current = list.head;
+
+      expect(current.next.next.val).toEqual(3);
+    });
+
+    it('successfully appends node to the end of the linkedList and returns the value', () => {
+      expect(list.append(-3)).toEqual(-3);
+    });
+
+    it('successfully appends multiple nodes to the end of the list', () => {
+      list.append(1);
+      list.append(2);
+      list.append(3);
+
+      let current = list.head;
+
+      expect(current.val).toEqual(1);
+      expect(current.next.val).toEqual(2);
+      expect(current.next.next.val).toEqual(3);
+    });   
+  });
+
+  describe('insertBefore', () => {
+    it('successfully inserts a node before a node located in the middle of a linked list', () => {
+      list.insert(1);
+      list.insert(2);
+      list.insert(4);
+      list.insertBefore(1, 3);
+
+      let current = list.head;
+
+      expect(current.next.next.val).toEqual(3);
+    });
+
+    it('successfully inserts a node before the first node of a linked list', () => {
+      list.append(1);
+      list.append(2);
+      list.append(3);
+      list.insertBefore(1, 0);
+
+      let current = list.head;
+
+      expect(current.val).toEqual(0);
+    });
+  });
+
+  describe('insertAfter', () => {
+    it('successfully inserts a node after a node located in the middle of a linked list', () => {
+      list.append(1);
+      list.append(2);
+      list.append(3);
+      list.insertAfter(2, 3);
+
+      let current = list.head;
+      
+      expect(current.next.next.val).toEqual(3);
+    });
+
+    it('successfully inserts a node after the last node of a linked list', () => {
+      list.append(1);
+      list.append(2);
+      list.append(3);
+      list.insertAfter(3, 4);
+
+      let current = list.head;
+
+      expect(current.next.next.next.val).toEqual(4);
+    });
   });
 });
