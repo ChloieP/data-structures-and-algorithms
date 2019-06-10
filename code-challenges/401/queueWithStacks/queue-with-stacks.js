f02150fc1bdb22bdd13fe954a346763fb6e29986
Chloie Parsons <chloieparsons@gmail.com>
2019-06-10 10:58:26 -0700

    Created PsuedoQueue class, constructor, and required methods (no content for them for now)

---
+++ code-challenges/401/queueWithStacks/queue-with-stacks.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const Stack = require('../stacks-and-queues').Stack;
+
+class PseudoQueue {
+  constructor() {
+    this.stackOne = new Stack(); 
+    this.stackTwo = new Stack();
+  }
+
+  enqueue(value) {
+
+  }
+
+  dequeue(value) {
+    
+  }
+
+}
