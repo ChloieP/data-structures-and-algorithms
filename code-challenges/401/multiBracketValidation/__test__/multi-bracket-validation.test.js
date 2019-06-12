2d8740124f82ad7552b051e352893187192d8e25
Chloie Parsons <chloieparsons@gmail.com>
2019-06-12 15:02:08 -0700

    Three passing tests

--- code-challenges/401/multiBracketValidation/__test__/multi-bracket-validation.test.js
+++ code-challenges/401/multiBracketValidation/__test__/multi-bracket-validation.test.js
@@ -3,9 +3,20 @@
 const balanced = require('../multi-bracket-validation');
 
 describe('multi-bracket-validation', () => {
-  let proof = '[{()]}'
+  let proof = '[[{}()]{}]';
+  it('should return false for an imbalanced bracket. )]', () => {
 
-  it('should return true for a balaned bracket', () => {
-    expect(balanced(proof)).toBeTruthy();
+    expect(balanced(proof)).toBe(false);
+  });
+
+  it('should fail for a nested string', () => {
+    let failure = '[[[[]){})';
+
+    expect(balanced(failure)).toBe(false);
+  });
+
+  it('should return false for a single bracket. {', () => {
+
+    expect(balanced(proof)).toBe(false);
   });
 })
--- code-challenges/401/multiBracketValidation/multi-bracket-validation.js
+++ code-challenges/401/multiBracketValidation/multi-bracket-validation.js
@@ -2,41 +2,34 @@
 
 const stack = require('../../401/stacksAndQueues/stacks-and-queues');
 
-let compare = '';
-
 let multiBracketValidation = (input) => {
-  let validationStack = new stack.Stack();
+  let validationStack = new stack.Stack;
 
-  for ( i = 0; i <input.length; i++ ) {
-    if (input[i] = ('[' || '{' || '(')) {
-      validationStack.push(input[i]);
-    };
-
-    if (input[i] = (']' || '}' || ')')) {
-      if (compare(']') = true) {
-        validationStack.pop();
-
-      } else { return false };
-
-      if (compare('}') = true) {
-        validationStack.pop();
-
-      } else { return false };
-
-      if (compare(')') = true) {
-        validationStack.pop();
-
-      } else { return false };
-    
-    return true;
+  for ( let i = 0; i <= input.length; i++ ) {
+    if (input.charAt(i) === '[' || input.charAt(i) === '{' || input.charAt(i) === '(') {
+      validationStack.push(input.charAt(i));
     }
 
-    if (validationStack.top = '') {
-        return true;
+    else if (input.charAt(i) === ']' || input.charAt(i) === '}' || input.charAt(i) === ')') {
+      if (validationStack.top === '[') {
+        if (input.charAt(i) !== ']') { return false; }
 
-      } else { return false};
+      } else if (validationStack.top === '{') {
+          if (input.charAt(i) !== '}') { return false; }
+
+      } else if (validationStack.top === '(') {
+        if (input.charAt(i) !== ')') { return false; }
+
+      } else { validationStack.top = validationStack.top.next;}
     }
+  }  
+  if(validationStack.top != null){
+    return false;
   }
+  else return true;
+};
+
+multiBracketValidation(stack);
 
 
 module.exports = multiBracketValidation;
