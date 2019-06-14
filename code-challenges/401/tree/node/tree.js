e5bb4b43d91d91b4f769c4ffbf94bcae6d2d58da
Chloie Parsons <chloieparsons@gmail.com>
2019-06-14 16:18:21 -0700

    File structure complete, BST class created, add method created

---
+++ code-challenges/401/tree/node/tree.js
@@ -0,0 +1,9 @@
+'use strict';
+
+class Node {
+  constuctor(value) {
+    this.value = value;
+    this.left = left || null;
+    this.right = right || null;
+  }
+}
---
+++ code-challenges/401/tree/tree.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const Node = require('./node/tree');
+
+class Node {
+  constuctor(value) {
+    this.value = value;
+    this.left = left || null;
+    this.right = right || null;
+  }
+}
