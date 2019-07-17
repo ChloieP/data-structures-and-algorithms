8d09801374c28e5cdff9bb061710777bd44b8eea
Chloie Parsons <chloieparsons@gmail.com>
2019-07-17 16:44:00 -0700

    WOW, tests finally pass. What a journey

--- code-challenges/401/depthFirst/depth-first.js
+++ code-challenges/401/depthFirst/depth-first.js
@@ -1,22 +1,39 @@
 'use strict';
 
-let depthFirst = (graph, root, list) => {
-  let list = [];
-
+function depthFirst(graph, root) {
   if(!graph || !root){return null;}
   if(!graph.getNodes().includes(root)){return null;}
-  if(root.visited === false){
-    list.push(root.value);
-    root.visited === true;
-  }
 
-  graph.getNeighbors(root).forEach(neighbor => {
-    if(neighbor.node.visited === false){
-      depthFirst(graph, neighbor.node, list);
+  const stack = [];
+  const visitedNodes = new Set();
+  const graphInitiated = graph.getNodes();
+  const startNode = graphInitiated[0];
+
+  let result = [];
+
+  stack.push(startNode);
+  visitedNodes.add(startNode);
+
+    while(stack.length){
+    const currentNode = stack.pop();
+
+    result.push(currentNode);
+
+    const neighbors = graph.getNeighbors(currentNode);
+
+    for(let edge of neighbors) {
+      const neighborNode = edge.node;
+
+      if(visitedNodes.has(neighborNode)){
+        continue; 
+      
+      } else {
+        visitedNodes.add(neighborNode);
+      }
+      stack.push(neighborNode);
     }
-  });
-
-  return list;
+  }
+  return result;
 };
 
 module.exports = depthFirst;
