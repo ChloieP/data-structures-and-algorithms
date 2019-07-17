4108cdbc7a77475e9f4d9c80826987f57d155afe
Chloie Parsons <chloieparsons@gmail.com>
2019-07-16 21:31:33 -0700

    Bug in tests, will figure out tomorrow

--- code-challenges/401/getEdge/__tests__/get-edge.test.js
+++ code-challenges/401/getEdge/__tests__/get-edge.test.js
@@ -1,19 +1,44 @@
-let graph = new graph.Graph();
-let PDX = graph.addNode('PDX');
-let JAX = graph.addNode('JAX');
-let Bahamas = graph.addNode('Bahamas');
-let CHI = graph.addNode('CHI');
-let Jamaica = graph.addNode('Jamaica');
+'use strict';
 
-graph.addBiDirectionalEdge(PDX, JAX, 400);
-graph.addBiDirectionalEdge(JAX, Bahamas, 100);
-graph.addBiDirectionalEdge(CHI, Jamaica, 350);
-graph.addBiDirectionalEdge(Bahamas, CHI, 300);
-graph.addBiDirectionalEdge(Bahamas, Jamaica, 150);
-graph.addBiDirectionalEdge(Bahamas, JAX, 100);
+const Graph = require('../../graph/graph');
+const getEdge = require('../get-edge');
 
-let happyCities = ['PDX', 'JAX', 'Bahamas', 'CHI'];
-let sadCities = ['Bahamas', 'PDX', 'CHI'];
+describe('Acceptable Flight Patterns', () => {
 
-console.log(getEdge(graph, happyCities));
-console.log(getEdge(graph, sadCities));
+  let graph;
+  let happyCities = ['PDX', 'JAX', 'Bahamas', 'CHI'];
+  let sadCities = ['Bahamas', 'PDX', 'CHI'];
+  let flightCancelled = ['PDX', 'PDX'];
+
+  beforeEach(() => {
+
+    graph = new Graph.Graph();
+
+    let PDX = graph.addValue('PDX');
+    let JAX = graph.addValue('JAX');
+    let Bahamas = graph.addValue('Bahamas');
+    let CHI = graph.addValue('CHI');
+    let Jamaica = graph.addValue('Jamaica');
+
+    graph.addBiDirectionalEdge(PDX, JAX, 400);
+    graph.addBiDirectionalEdge(JAX, Bahamas, 100);
+    graph.addBiDirectionalEdge(CHI, Jamaica, 350);
+    graph.addBiDirectionalEdge(Bahamas, CHI, 300);
+    graph.addBiDirectionalEdge(Bahamas, Jamaica, 150);
+    graph.addBiDirectionalEdge(Bahamas, JAX, 100);
+
+  });
+
+  it('can find the Bahamas and be happy!', () => {
+    expect(getEdge(graph, happyCities)).toEqual('true $500');
+  });
+
+  it('cannot find Jamaica and is sad', () => {
+    expect(getEdge(graph, sadCities)).toEqual('false $0');
+  });
+
+  it('returns false if your flight is cancelled', () => {
+    expect(getEdge(graph, flightCancelled)).toEqual('false $0')
+  });
+});
+
--- code-challenges/401/getEdge/get-edge.js
+++ code-challenges/401/getEdge/get-edge.js
@@ -1,36 +1,34 @@
 'use strict';
 
-const graph = require('../graph/graph');
-
 function getEdge(graph, cities){
+  
   let counter = 0;
   let current;
   let graphCities = graph.getNodes();
 
-  for(let i = 0; i < graphCities.length; i++) {
-    if(graphCities[i].value === cities[0]) {
+  for(let i = 0; i < graphCities.length; i++){
+    if(graphCities[i].value === cities[0]){
       current = graphCities[i];
     }
   }
 
-  for(let i = 1; i < array.length; i++) {
-    let neighbor = graph.getNeighbors(current);
-    let isPresent = false;
+  for(let i = 1; i < cities.length; i++){
 
-    for(let j = 0; j < neighbor.length; j++) {
-      if(neighbor[j].node.value === array[i]) {
+    let neighbor = graph.getNeighbors(current);
+    
+    let isPresent = false;
+    for(let j = 0; j < neighbor.length; j++){
+      if(neighbor[j].node.value === array[i]){
         counter += neighbor[j].weight;
         isPresent = true;
         current = neighbor[j].node;
         break;
       }
     }
-
-    if(!isPresent) {
-      return false + ': $0';
+    if(!isPresent){
+      return false + ' $0';
     }
   }
-
   return `true: $${counter}`;
 }
 
